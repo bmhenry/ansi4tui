813434267781f912e17eec6c85dc9655c7a45dec
Brandon Henry <brandon@bhenry.dev>
2021-02-18 00:02:25 -0500

    add example, add LICENSE file (same license as was already specified in Cargo.toml)

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ let c = Command::new("ls")
 
 let text = ansi4tui::bytes_to_text(c.stdout);
 ```
+
+For a simple usage example, see [the example file](./examples/simple.rs). You can run the example with `cargo run --example simple`, and close the display by pressing `q`. It assumes you're on a system with `/etc/hosts`, so change the file if you want to run the example and that file isn't available.
---
+++ examples/simple.rs
@@ -0,0 +1,53 @@
+mod util;
+
+use std::process::Command;
+
+use util::event::{Event, Events};
+use std::{error::Error, io};
+use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
+use tui::{
+    backend::TermionBackend,
+    style::Style,
+    widgets::{Block, Borders, Paragraph},
+    Terminal,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Terminal initialization
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let events = Events::new();
+
+    loop {
+        terminal.draw(|f| {
+            let size = f.size();
+
+            // run a Command and retrieve stdout as bytes
+            let c = Command::new("bat")
+                .arg("/etc/hosts")
+                .output()
+                .unwrap();
+
+            // render bytes to tui `Text`, converting ansi escape codes to tui `Style`s
+            let text = ansi4tui::bytes_to_text(c.stdout);
+
+            // display text
+            let paragraph = Paragraph::new(text)
+                .style(Style::default())
+                .block(Block::default().title("Preview").borders(Borders::ALL));
+
+            f.render_widget(paragraph, size);
+        })?;
+
+        if let Event::Input(key) = events.next()? {
+            if key == Key::Char('q') {
+                break;
+            }
+        }
+    }
+    Ok(())
+}
